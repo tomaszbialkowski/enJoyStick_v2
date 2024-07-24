6d08c2a3b457c522278e1c0ec2405be99c793776
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 21:18:38 +0200

    some small changes to app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import RouterConfig from "./router/RouterConfig";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfo, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 import Header from "./components/Header";
 import Logo from "./components/Logo";
@@ -15,7 +16,6 @@ import Lists from "./components/Lists";
 import Footer from "./components/Footer";
 import ModalInfo from "./components/ModalInfo";
 import Button from "./components/Button";
-import { faInfo, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const modalInfo = useSelector((state) => state.info);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
